5c57d89b2ce0843c199ec76b71db9ba645ad2724
Maximusbarcz <maxim.baranek@gmail.com>
2023-07-05 14:48:59 +0200

    Nothing new it seems...

--- src/main/java/dev/mayaqq/estrogen/registry/ParticleRegistry.java
+++ src/main/java/dev/mayaqq/estrogen/registry/ParticleRegistry.java
@@ -1,6 +1,9 @@
 package dev.mayaqq.estrogen.registry;
 
 import dev.mayaqq.estrogen.Estrogen;
+import dev.mayaqq.estrogen.registry.particles.DashParticle;
+import net.fabricmc.fabric.api.client.particle.v1.FabricSpriteProvider;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
 import net.minecraft.particle.DefaultParticleType;
 import net.minecraft.util.registry.Registry;
