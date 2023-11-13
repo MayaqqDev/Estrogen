821f05329f73f40c528ab32c652e81d7ec7c870e
mayaqqdev <i@mayaqq.dev>
2023-11-13 19:14:19 +0100

    1.19.2 Fixed Ears compat being broken Removed fabric_loader_dependencies.json from .gitignore added createreibugfix to dev env and a warning to the user if rei is installed without it

--- src/main/java/dev/mayaqq/estrogen/client/EstrogenClient.java
+++ src/main/java/dev/mayaqq/estrogen/client/EstrogenClient.java
@@ -6,6 +6,8 @@ import dev.mayaqq.estrogen.registry.client.*;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.loader.api.FabricLoader;
 
+import static dev.mayaqq.estrogen.Estrogen.LOGGER;
+
 public class EstrogenClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
@@ -21,6 +23,14 @@ public class EstrogenClient implements ClientModInitializer {
         // mod compat
         if (FabricLoader.getInstance().isModLoaded("ears")) {
             EarsCompat.boob();
+        }
+        if (FabricLoader.getInstance().isModLoaded("roughlyenoughitems") && !FabricLoader.getInstance().isModLoaded("createreibugfix")) {
+            LOGGER.warn("--------------------------------------------------------------------------------------------------------------------------");
+            LOGGER.warn("");
+            LOGGER.warn("[ESTROGEN] Roughly Enough Items is installed without Create REI Bugfix! This will cause issues with some Estrogen Recipes.");
+            LOGGER.warn("[ESTROGEN] Please install Create REI Bugfix to fix this issue: https://modrinth.com/mod/createfabricreibugfix");
+            LOGGER.warn("");
+            LOGGER.warn("--------------------------------------------------------------------------------------------------------------------------");
         }
 
     }
