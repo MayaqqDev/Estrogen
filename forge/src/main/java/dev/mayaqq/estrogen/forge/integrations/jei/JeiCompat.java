7608ebf541536818d2aef95faa5855104125b0d2
Dorina Kovacs <princessdorinakovacs@gmail.com>
2024-01-17 23:05:39 +0100

    Forge JEI

---
+++ forge/src/main/java/dev/mayaqq/estrogen/forge/integrations/jei/JeiCompat.java
@@ -0,0 +1,262 @@
+package dev.mayaqq.estrogen.forge.integrations.jei;
+
+import com.simibubi.create.Create;
+import com.simibubi.create.compat.jei.*;
+import com.simibubi.create.compat.jei.category.CreateRecipeCategory;
+import com.simibubi.create.foundation.config.ConfigBase.ConfigBool;
+import com.simibubi.create.foundation.recipe.IRecipeTypeInfo;
+import com.simibubi.create.foundation.utility.Lang;
+import com.simibubi.create.infrastructure.config.AllConfigs;
+import com.simibubi.create.infrastructure.config.CRecipes;
+import dev.architectury.core.fluid.ArchitecturyFlowingFluid;
+import dev.mayaqq.estrogen.forge.integrations.jei.categories.CentrifugingCategory;
+import dev.mayaqq.estrogen.registry.common.EstrogenBlocks;
+import dev.mayaqq.estrogen.registry.common.EstrogenFluids;
+import dev.mayaqq.estrogen.registry.common.EstrogenRecipes;
+import dev.mayaqq.estrogen.registry.common.recipes.CentrifugingRecipe;
+import mezz.jei.api.JeiPlugin;
+import mezz.jei.api.constants.RecipeTypes;
+import mezz.jei.api.forge.ForgeTypes;
+import mezz.jei.api.gui.drawable.IDrawable;
+import mezz.jei.api.helpers.IPlatformFluidHelper;
+import mezz.jei.api.recipe.category.IRecipeCategory;
+import mezz.jei.api.registration.*;
+import mezz.jei.api.runtime.IIngredientManager;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Recipe;
+import net.minecraft.world.item.crafting.RecipeType;
+import net.minecraft.world.level.ItemLike;
+
+import javax.annotation.Nonnull;
+import javax.annotation.ParametersAreNonnullByDefault;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+import static dev.mayaqq.estrogen.Estrogen.id;
+
+@JeiPlugin
+@SuppressWarnings("unused")
+@ParametersAreNonnullByDefault
+public class JeiCompat extends CreateJEI {
+
+    private static final ResourceLocation ID = id("jei_plugin");
+
+    private final List<CreateRecipeCategory<?>> allCategories = new ArrayList<>();
+    private IIngredientManager ingredientManager;
+
+    private void loadCategories() {
+        allCategories.clear();
+
+        CreateRecipeCategory<?>
+
+                centrifuging = builder(CentrifugingRecipe.class)
+                        .addTypedRecipes(EstrogenRecipes.CENTRIFUGING)
+                        .catalyst(EstrogenBlocks.CENTRIFUGE::get)
+                        .itemIcon(EstrogenBlocks.CENTRIFUGE.get())
+                        .emptyBackground(177, 70)
+                        .build("centrifuging", CentrifugingCategory::new);
+    }
+
+    private <T extends Recipe<?>> CategoryBuilder<T> builder(Class<? extends T> recipeClass) {
+        return new CategoryBuilder<>(recipeClass);
+    }
+
+    @Override
+    @Nonnull
+    public ResourceLocation getPluginUid() {
+        return ID;
+    }
+
+    @Override
+    public void registerCategories(IRecipeCategoryRegistration registration) {
+        loadCategories();
+        registration.addRecipeCategories(allCategories.toArray(IRecipeCategory[]::new));
+    }
+
+    @Override
+    public void registerRecipes(IRecipeRegistration registration) {
+        ingredientManager = registration.getIngredientManager();
+
+        allCategories.forEach(c -> c.registerRecipes(registration));
+
+        registration.addRecipes(RecipeTypes.CRAFTING, ToolboxColoringRecipeMaker.createRecipes().toList());
+    }
+
+    @Override
+    public void registerRecipeCatalysts(IRecipeCatalystRegistration registration) {
+        allCategories.forEach(c -> c.registerCatalysts(registration));
+    }
+
+    @Override
+    public void registerRecipeTransferHandlers(IRecipeTransferRegistration registration) {
+        registration.addRecipeTransferHandler(new BlueprintTransferHandler(), RecipeTypes.CRAFTING);
+    }
+
+    @Override
+    public <T> void registerFluidSubtypes(ISubtypeRegistration registration, IPlatformFluidHelper<T> platformFluidHelper) {
+        PotionFluidSubtypeInterpreter interpreter = new PotionFluidSubtypeInterpreter();
+        ArchitecturyFlowingFluid liquidEstrogen = EstrogenFluids.LIQUID_ESTROGEN.get();
+        registration.registerSubtypeInterpreter(ForgeTypes.FLUID_STACK, liquidEstrogen.getSource(), interpreter);
+        registration.registerSubtypeInterpreter(ForgeTypes.FLUID_STACK, liquidEstrogen.getFlowing(), interpreter);
+    }
+
+    private class CategoryBuilder<T extends Recipe<?>> {
+        private final Class<? extends T> recipeClass;
+        private Predicate<CRecipes> predicate = cRecipes -> true;
+
+        private IDrawable background;
+        private IDrawable icon;
+
+        private final List<Consumer<List<T>>> recipeListConsumers = new ArrayList<>();
+        private final List<Supplier<? extends ItemStack>> catalysts = new ArrayList<>();
+
+        public CategoryBuilder(Class<? extends T> recipeClass) {
+            this.recipeClass = recipeClass;
+        }
+
+        public CategoryBuilder<T> enableIf(Predicate<CRecipes> predicate) {
+            this.predicate = predicate;
+            return this;
+        }
+
+        public CategoryBuilder<T> enableWhen(Function<CRecipes, ConfigBool> configValue) {
+            predicate = c -> configValue.apply(c).get();
+            return this;
+        }
+
+        public CategoryBuilder<T> addRecipeListConsumer(Consumer<List<T>> consumer) {
+            recipeListConsumers.add(consumer);
+            return this;
+        }
+
+        public CategoryBuilder<T> addRecipes(Supplier<Collection<? extends T>> collection) {
+            return addRecipeListConsumer(recipes -> recipes.addAll(collection.get()));
+        }
+
+        public CategoryBuilder<T> addAllRecipesIf(Predicate<Recipe<?>> pred) {
+            return addRecipeListConsumer(recipes -> consumeAllRecipes(recipe -> {
+                if (pred.test(recipe)) {
+                    recipes.add((T) recipe);
+                }
+            }));
+        }
+
+        public CategoryBuilder<T> addAllRecipesIf(Predicate<Recipe<?>> pred, Function<Recipe<?>, T> converter) {
+            return addRecipeListConsumer(recipes -> consumeAllRecipes(recipe -> {
+                if (pred.test(recipe)) {
+                    recipes.add(converter.apply(recipe));
+                }
+            }));
+        }
+
+        public CategoryBuilder<T> addTypedRecipes(IRecipeTypeInfo recipeTypeEntry) {
+            return addTypedRecipes(recipeTypeEntry::getType);
+        }
+
+        public CategoryBuilder<T> addTypedRecipes(Supplier<RecipeType<? extends T>> recipeType) {
+            return addRecipeListConsumer(recipes -> JeiCompat.<T>consumeTypedRecipes(recipes::add, recipeType.get()));
+        }
+
+        public CategoryBuilder<T> addTypedRecipes(Supplier<RecipeType<? extends T>> recipeType, Function<Recipe<?>, T> converter) {
+            return addRecipeListConsumer(recipes -> JeiCompat.<T>consumeTypedRecipes(recipe -> recipes.add(converter.apply(recipe)), recipeType.get()));
+        }
+
+        public CategoryBuilder<T> addTypedRecipesIf(Supplier<RecipeType<? extends T>> recipeType, Predicate<Recipe<?>> pred) {
+            return addRecipeListConsumer(recipes -> JeiCompat.<T>consumeTypedRecipes(recipe -> {
+                if (pred.test(recipe)) {
+                    recipes.add(recipe);
+                }
+            }, recipeType.get()));
+        }
+
+        public CategoryBuilder<T> addTypedRecipesExcluding(Supplier<RecipeType<? extends T>> recipeType,
+                                                           Supplier<RecipeType<? extends T>> excluded) {
+            return addRecipeListConsumer(recipes -> {
+                List<Recipe<?>> excludedRecipes = getTypedRecipes(excluded.get());
+                JeiCompat.<T>consumeTypedRecipes(recipe -> {
+                    for (Recipe<?> excludedRecipe : excludedRecipes) {
+                        if (doInputsMatch(recipe, excludedRecipe)) {
+                            return;
+                        }
+                    }
+                    recipes.add(recipe);
+                }, recipeType.get());
+            });
+        }
+
+        public CategoryBuilder<T> removeRecipes(Supplier<RecipeType<? extends T>> recipeType) {
+            return addRecipeListConsumer(recipes -> {
+                List<Recipe<?>> excludedRecipes = getTypedRecipes(recipeType.get());
+                recipes.removeIf(recipe -> {
+                    for (Recipe<?> excludedRecipe : excludedRecipes)
+                        if (doInputsMatch(recipe, excludedRecipe) && doOutputsMatch(recipe, excludedRecipe))
+                            return true;
+                    return false;
+                });
+            });
+        }
+
+        public CategoryBuilder<T> catalystStack(Supplier<ItemStack> supplier) {
+            catalysts.add(supplier);
+            return this;
+        }
+
+        public CategoryBuilder<T> catalyst(Supplier<ItemLike> supplier) {
+            return catalystStack(() -> new ItemStack(supplier.get()
+                    .asItem()));
+        }
+
+        public CategoryBuilder<T> icon(IDrawable icon) {
+            this.icon = icon;
+            return this;
+        }
+
+        public CategoryBuilder<T> itemIcon(ItemLike item) {
+            icon(new ItemIcon(() -> new ItemStack(item)));
+            return this;
+        }
+
+        public CategoryBuilder<T> doubleItemIcon(ItemLike item1, ItemLike item2) {
+            icon(new DoubleItemIcon(() -> new ItemStack(item1), () -> new ItemStack(item2)));
+            return this;
+        }
+
+        public CategoryBuilder<T> background(IDrawable background) {
+            this.background = background;
+            return this;
+        }
+
+        public CategoryBuilder<T> emptyBackground(int width, int height) {
+            background(new EmptyBackground(width, height));
+            return this;
+        }
+
+        public CreateRecipeCategory<T> build(String name, CreateRecipeCategory.Factory<T> factory) {
+            Supplier<List<T>> recipesSupplier;
+            if (predicate.test(AllConfigs.server().recipes)) {
+                recipesSupplier = () -> {
+                    List<T> recipes = new ArrayList<>();
+                    for (Consumer<List<T>> consumer : recipeListConsumers)
+                        consumer.accept(recipes);
+                    return recipes;
+                };
+            } else {
+                recipesSupplier = () -> Collections.emptyList();
+            }
+
+            CreateRecipeCategory.Info<T> info = new CreateRecipeCategory.Info<>(
+                    new mezz.jei.api.recipe.RecipeType<>(Create.asResource(name), recipeClass),
+                    Lang.translateDirect("recipe." + name), background, icon, recipesSupplier, catalysts);
+            CreateRecipeCategory<T> category = factory.create(info);
+            allCategories.add(category);
+            return category;
+        }
+    }
+}
