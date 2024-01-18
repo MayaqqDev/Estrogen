0c175484016704c0bca56d4b6ce39da323dd3a53
mayaqqdev <i@mayaqq.dev>
2024-01-18 21:51:11 +0100

    Fixed PlayerLookup

--- common/src/main/java/dev/mayaqq/estrogen/utils/PlayerLookup.java
+++ common/src/main/java/dev/mayaqq/estrogen/utils/PlayerLookup.java
@@ -1,14 +1,34 @@
 package dev.mayaqq.estrogen.utils;
 
+import net.minecraft.server.level.ChunkMap;
+import net.minecraft.server.level.ServerChunkCache;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerPlayerConnection;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.chunk.ChunkSource;
 
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class PlayerLookup {
     public static Collection<ServerPlayer> tracking(Entity entity) {
-        //TODO This has to be redone once I find a better way without fapi.
-        return Objects.requireNonNull(entity.getServer()).getPlayerList().getPlayers();
+        Objects.requireNonNull(entity, "Entity cannot be null");
+        ChunkSource manager = entity.level().getChunkSource();
+
+        if (manager instanceof ServerChunkCache) {
+            ChunkMap storage = ((ServerChunkCache) manager).chunkMap;
+            ChunkMap.TrackedEntity tracker = storage.entityMap.get(entity.getId());
+
+            // return an immutable collection to guard against accidental removals.
+            if (tracker != null) {
+                return tracker.seenBy.stream().map(ServerPlayerConnection::getPlayer).collect(Collectors.toUnmodifiableSet());
+            }
+
+            return Collections.emptySet();
+        }
+
+        throw new IllegalArgumentException("Only supported on server worlds!");
     }
 }
