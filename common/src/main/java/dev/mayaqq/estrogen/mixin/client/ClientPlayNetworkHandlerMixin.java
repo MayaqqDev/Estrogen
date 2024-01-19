d9c6b8783c8301cf53307cb1c5e5a14cf9383158
mayaqqdev <i@mayaqq.dev>
2024-01-19 19:54:15 +0100

    fixed uwufying

--- common/src/main/java/dev/mayaqq/estrogen/mixin/client/ClientPlayNetworkHandlerMixin.java
+++ common/src/main/java/dev/mayaqq/estrogen/mixin/client/ClientPlayNetworkHandlerMixin.java
@@ -1,13 +1,15 @@
 package dev.mayaqq.estrogen.mixin.client;
 
+import dev.mayaqq.estrogen.Estrogen;
 import dev.mayaqq.estrogen.registry.common.EstrogenEnchantments;
-import dev.mayaqq.estrogen.registry.common.EstrogenTags;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import net.minecraft.client.player.LocalPlayer;
-import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 
@@ -15,22 +17,19 @@ import static dev.mayaqq.estrogen.utils.UwUfy.uwufyString;
 
 @Mixin(ClientPacketListener.class)
 public class ClientPlayNetworkHandlerMixin {
+    @Shadow @Final private static Logger LOGGER;
+
     @ModifyVariable(
             method = "sendChat(Ljava/lang/String;)V",
             at = @At(value = "HEAD"),
-            ordinal = 0, argsOnly = true
+            ordinal = 0,
+            argsOnly = true
     )
     private String modifyMessage(String message) {
         LocalPlayer player = Minecraft.getInstance().player;
-        if (player.getInventory().contains(EstrogenTags.UWUFYING)) {
+        if (EnchantmentHelper.getEnchantments(player.getInventory().getArmor(3)).containsKey(EstrogenEnchantments.UWUFYING_CURSE.get())) {
             message = uwufyString(message);
-        } else {
-            for (ItemStack stack : player.getArmorSlots()) {
-                if (EnchantmentHelper.getEnchantments(stack).containsKey(EstrogenEnchantments.UWUFYING_CURSE)) {
-                    message = uwufyString(message);
-                    break;
-                }
-            }
+
         }
         return message;
     }
