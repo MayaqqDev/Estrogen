6e6aec9b70bc8fd44120419c995bbcd1f365c82f
mayaqqdev <i@mayaqq.dev>
2024-01-22 23:19:22 +0100

    fixed bad client initilization on forge I think I hope please tell me i fixed it or im going to explode.

--- forge/src/main/java/dev/mayaqq/estrogen/forge/client/EstrogenForgeClient.java
+++ forge/src/main/java/dev/mayaqq/estrogen/forge/client/EstrogenForgeClient.java
@@ -1,12 +1,13 @@
 package dev.mayaqq.estrogen.forge.client;
 
 import dev.mayaqq.estrogen.Estrogen;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import dev.mayaqq.estrogen.client.EstrogenClient;
 
-@Mod.EventBusSubscriber(modid = Estrogen.MOD_ID)
+@Mod.EventBusSubscriber(modid = Estrogen.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class EstrogenForgeClient {
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event) {
