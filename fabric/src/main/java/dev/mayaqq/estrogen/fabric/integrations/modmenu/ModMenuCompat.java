c434d9c20718b875789022f0bd52a3af41bcb920
mayaqqdev <i@mayaqq.dev>
2024-01-26 17:34:25 +0100

    1.19.2 progress

---
+++ fabric/src/main/java/dev/mayaqq/estrogen/fabric/integrations/modmenu/ModMenuCompat.java
@@ -0,0 +1,14 @@
+package dev.mayaqq.estrogen.fabric.integrations.modmenu;
+
+import com.simibubi.create.foundation.config.ui.BaseConfigScreen;
+import com.terraformersmc.modmenu.api.ConfigScreenFactory;
+import com.terraformersmc.modmenu.api.ModMenuApi;
+import dev.mayaqq.estrogen.Estrogen;
+
+public class ModMenuCompat implements ModMenuApi {
+    public ConfigScreenFactory<?> getModConfigScreenFactory() {
+        return (screen) -> {
+            return new BaseConfigScreen(screen, Estrogen.MOD_ID);
+        };
+    }
+}
--- forge/src/main/java/dev/mayaqq/estrogen/forge/EstrogenForge.java
+++ forge/src/main/java/dev/mayaqq/estrogen/forge/EstrogenForge.java
@@ -1,9 +1,12 @@
 package dev.mayaqq.estrogen.forge;
 
 import com.simibubi.create.foundation.config.ConfigBase;
+import com.simibubi.create.foundation.config.ui.BaseConfigScreen;
 import dev.architectury.platform.forge.EventBuses;
 import dev.mayaqq.estrogen.Estrogen;
 import dev.mayaqq.estrogen.config.EstrogenConfig;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -27,6 +30,10 @@ public class EstrogenForge {
         EstrogenConfig.register();
         for (Map.Entry<ModConfig.Type, ConfigBase> pair : EstrogenConfig.CONFIGS.entrySet())
             ModLoadingContext.get().registerConfig(pair.getKey(), pair.getValue().specification);
+
+        // Config Button
+        ModLoadingContext.get().getActiveContainer().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> new BaseConfigScreen(screen, MOD_ID)));
 
         // Init Estrogen main class
         Estrogen.init();
