1bdd74bb8d2175b0be078b7eebeda530d259f1a6
mayaqqdev <i@mayaqq.dev>
2024-01-26 17:56:12 +0100

    Fixed Maya idiocy

--- forge/src/main/java/dev/mayaqq/estrogen/forge/client/EstrogenForgeClient.java
+++ forge/src/main/java/dev/mayaqq/estrogen/forge/client/EstrogenForgeClient.java
@@ -1,18 +1,27 @@
 package dev.mayaqq.estrogen.forge.client;
 
+import com.simibubi.create.foundation.config.ui.BaseConfigScreen;
 import dev.mayaqq.estrogen.Estrogen;
 import dev.mayaqq.estrogen.client.EstrogenClient;
 import dev.mayaqq.estrogen.registry.client.EstrogenRenderer;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.client.ConfigScreenHandler;
 import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+
+import static dev.mayaqq.estrogen.Estrogen.MOD_ID;
 
 @Mod.EventBusSubscriber(modid = Estrogen.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class EstrogenForgeClient {
     @SubscribeEvent
     public static void clientSetup(FMLClientSetupEvent event) {
+        // Config Button
+        ModLoadingContext.get().getActiveContainer().registerExtensionPoint(ConfigScreenHandler.ConfigScreenFactory.class,
+                () -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> new BaseConfigScreen(screen, MOD_ID)));
+        // Common Client Init
         EstrogenClient.init();
     }
 
