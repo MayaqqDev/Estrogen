a488119926028b14139a308386e8ab75add5d96d
mayaqqdev <i@mayaqq.dev>
2024-01-28 15:17:49 +0100

    Fixed inconsistent model registry crash on forge (again)

--- forge/src/main/java/dev/mayaqq/estrogen/forge/client/EstrogenForgeClient.java
+++ forge/src/main/java/dev/mayaqq/estrogen/forge/client/EstrogenForgeClient.java
@@ -3,10 +3,8 @@ package dev.mayaqq.estrogen.forge.client;
 import com.simibubi.create.foundation.config.ui.BaseConfigScreen;
 import dev.mayaqq.estrogen.Estrogen;
 import dev.mayaqq.estrogen.client.EstrogenClient;
-import dev.mayaqq.estrogen.registry.client.EstrogenRenderer;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.client.event.ModelEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
@@ -23,11 +21,5 @@ public class EstrogenForgeClient {
                 () -> new ConfigScreenHandler.ConfigScreenFactory((minecraft, screen) -> new BaseConfigScreen(screen, MOD_ID)));
         // Common Client Init
         EstrogenClient.init();
-    }
-
-    @SubscribeEvent
-    public static void modelRegistry(ModelEvent.RegisterAdditional event) {
-        // Has to have this because if it doesn't, it breaks with cobblemon (for some reason)
-        EstrogenRenderer.register();
     }
 }
