e1f07a14eff2756e11b1b158a3c33bce8bf814da
mayaqqdev <i@mayaqq.dev>
2024-02-18 17:43:06 +0100

    optimized imports

--- common/src/main/java/dev/mayaqq/estrogen/registry/items/EstrogenPatchesItem.java
+++ common/src/main/java/dev/mayaqq/estrogen/registry/items/EstrogenPatchesItem.java
@@ -5,16 +5,11 @@ import dev.mayaqq.estrogen.config.EstrogenConfig;
 import dev.mayaqq.estrogen.registry.EstrogenEffects;
 import earth.terrarium.baubly.common.Bauble;
 import earth.terrarium.baubly.common.SlotInfo;
-import net.minecraft.core.BlockPos;
 import net.minecraft.nbt.CompoundTag;
-import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
 
 public class EstrogenPatchesItem extends Item implements Bauble {
