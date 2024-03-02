6c67a861cd9cb2dcc35ae81aaf9905db2f47e12b
mayaqqdev <i@mayaqq.dev>
2024-03-02 21:20:51 +0100

    Fixed Ponder scenes

--- common/src/main/java/dev/mayaqq/estrogen/registry/EstrogenPonderScenes.java
+++ common/src/main/java/dev/mayaqq/estrogen/registry/EstrogenPonderScenes.java
@@ -1,16 +1,16 @@
 package dev.mayaqq.estrogen.registry;
 
 import com.simibubi.create.foundation.ponder.PonderRegistrationHelper;
+import com.simibubi.create.infrastructure.ponder.AllPonderTags;
+import dev.mayaqq.estrogen.Estrogen;
 import dev.mayaqq.estrogen.registry.ponders.CentrifugeStoryboard;
 
 public class EstrogenPonderScenes {
 
-    static final PonderRegistrationHelper HELPER = new PonderRegistrationHelper("estrogen");
+    static final PonderRegistrationHelper HELPER = new PonderRegistrationHelper(Estrogen.MOD_ID);
 
     public static void register() {
-        //TODO: BROKEN
-        HELPER.forComponents()
-                .addStoryBoard("centrifuge/intro", CentrifugeStoryboard::centrifugeStoryboardIntro)
-                .addStoryBoard("centrifuge/basic", CentrifugeStoryboard::centrifugeStoryboardBasic);
+        HELPER.addStoryBoard(EstrogenItems.CENTRIFUGE.getId(), Estrogen.id("centrifuge/intro"), new CentrifugeStoryboard.Intro(), AllPonderTags.KINETIC_APPLIANCES);
+        HELPER.addStoryBoard(EstrogenItems.CENTRIFUGE.getId(), Estrogen.id("centrifuge/basic"), new CentrifugeStoryboard.Basic(), AllPonderTags.KINETIC_APPLIANCES);
     }
 }
