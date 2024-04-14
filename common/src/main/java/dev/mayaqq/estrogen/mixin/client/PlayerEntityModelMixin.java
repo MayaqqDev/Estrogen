87167bd66d20087d800014ab5386113e761e80d8
Dorina Kovacs <princessdorinakovacs@gmail.com>
2024-04-14 23:12:40 +0200

    Chest feature skin outer layer

--- common/src/main/java/dev/mayaqq/estrogen/mixin/client/PlayerEntityModelMixin.java
+++ common/src/main/java/dev/mayaqq/estrogen/mixin/client/PlayerEntityModelMixin.java
@@ -30,6 +30,7 @@ import net.minecraft.client.renderer.texture.TextureAtlasSprite;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.PlayerModelPart;
 import net.minecraft.world.item.ArmorItem;
 import net.minecraft.world.item.ArmorMaterial;
 import net.minecraft.world.item.ItemStack;
@@ -56,6 +57,9 @@ public class PlayerEntityModelMixin<T extends LivingEntity> extends HumanoidMode
     private ModelPart estrogen$boobs;
 
     @Unique
+    private ModelPart estrogen$boobJacket;
+
+    @Unique
     private BoobArmorRenderer estrogen$boobArmor;
 
     public PlayerEntityModelMixin(ModelPart root) {
@@ -69,12 +73,14 @@ public class PlayerEntityModelMixin<T extends LivingEntity> extends HumanoidMode
     private static MeshDefinition estrogen$getTextureModelData(MeshDefinition original) {
         PartDefinition modelPartData = original.getRoot();
         modelPartData.addOrReplaceChild("boobs", CubeListBuilder.create().addBox("boobs", -4.0F, 0F, 0F, 8, 2, 2, CubeDeformation.NONE, 18, 22), PartPose.ZERO);
+        modelPartData.addOrReplaceChild("boobs_jacket", CubeListBuilder.create().addBox("boobs_jacket", -4.0F, 0F, 0F, 8, 2, 2, new CubeDeformation(0.25f), 18, 38), PartPose.ZERO);
         return original;
     }
 
     @Inject(method = "<init>", at = @At("RETURN"))
     private void estrogen$init(ModelPart root, boolean thinArms, CallbackInfo ci) {
         estrogen$boobs = root.getChild("boobs");
+        estrogen$boobJacket = root.getChild("boobs_jacket");
         estrogen$boobArmor = new BoobArmorRenderer(Collections.singletonList(new BoobArmorRenderer.BoobArmorModel(18, 23, 8, 2, 2, false, 64.0F, 32.0F)));
     }
 
@@ -92,6 +98,15 @@ public class PlayerEntityModelMixin<T extends LivingEntity> extends HumanoidMode
         this.estrogen$boobs.yScale = (1 + size * 2.0F * (1 + amplifier)) / 2.0F;
         this.estrogen$boobs.zScale = (1 + size * 2.5F * (1 + amplifier)) / 2.0F;
         this.estrogen$boobs.render(matrices, vertices, light, overlay);
+
+        this.estrogen$boobJacket.visible = player.isModelPartShown(PlayerModelPart.JACKET);
+        this.estrogen$boobJacket.copyFrom(this.body);
+        this.estrogen$boobJacket.xRot = this.estrogen$boobs.xRot;
+        this.estrogen$boobJacket.offsetPos(new Vector3f(0.0F, 4.0F + size * 0.864F * (1 + amplifier) + yOffset, -1.9F + size * -1.944F * (1 + amplifier)).rotate(bodyRotation));
+        this.estrogen$boobJacket.yScale = this.estrogen$boobs.yScale;
+        this.estrogen$boobJacket.zScale = this.estrogen$boobs.zScale;
+        this.estrogen$boobJacket.render(matrices, vertices, light, overlay);
+
     }
 
     @Override
@@ -149,6 +164,7 @@ public class PlayerEntityModelMixin<T extends LivingEntity> extends HumanoidMode
     @Inject(method = "setAllVisible", at = @At("RETURN"))
     private void estrogen$setVisible(boolean visible, CallbackInfo ci) {
         this.estrogen$boobs.visible = visible;
+        this.estrogen$boobJacket.visible = visible;
         this.estrogen$boobArmor.visible = visible;
     }
 }
