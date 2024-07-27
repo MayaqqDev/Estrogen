843cc7bd127d3601c6adc4f0f317b039e152cd2e
MayaqqDev <i@mayaqq.dev>
2024-07-27 02:16:40 +0200

    nothing for testosterone yet, just made the branch and effect class.

---
+++ common/src/main/java/dev/mayaqq/estrogen/registry/effects/TestosteroneEffect.java
@@ -0,0 +1,10 @@
+package dev.mayaqq.estrogen.registry.effects;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class TestosteroneEffect extends MobEffect {
+    protected TestosteroneEffect(MobEffectCategory category, int color) {
+        super(category, color);
+    }
+}
