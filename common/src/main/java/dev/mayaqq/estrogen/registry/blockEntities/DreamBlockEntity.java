dd3cb31a2850dd5129bf164e2afdc4438f020415
MayaqqDev <i@mayaqq.dev>
2024-08-06 17:04:18 +0200

    removed unused constructor, fixed blahaj compat

--- common/src/main/java/dev/mayaqq/estrogen/registry/blockEntities/DreamBlockEntity.java
+++ common/src/main/java/dev/mayaqq/estrogen/registry/blockEntities/DreamBlockEntity.java
@@ -6,17 +6,12 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.entity.BlockEntity;
-import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
 public class DreamBlockEntity extends BlockEntity {
     @Environment(EnvType.CLIENT)
     DreamBlockTexture texture;
-
-    public DreamBlockEntity(BlockEntityType<?> type, BlockPos pos, BlockState blockState) {
-        super(type, pos, blockState);
-    }
 
     public DreamBlockEntity(BlockPos pos, BlockState state) {
         super(EstrogenBlockEntities.DREAM_BLOCK.get(), pos, state);
