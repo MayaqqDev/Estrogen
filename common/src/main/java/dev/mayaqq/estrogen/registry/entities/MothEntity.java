a9a1aa2776bd8d3364e8d63c1b024d3f62a46b93
MayaqqDev <i@mayaqq.dev>
2024-08-14 21:39:34 +0200

    maya went insane and lost her mind.

--- common/src/main/java/dev/mayaqq/estrogen/registry/entities/MothEntity.java
+++ common/src/main/java/dev/mayaqq/estrogen/registry/entities/MothEntity.java
@@ -214,7 +214,7 @@ public class MothEntity extends Animal implements FlyingAnimal, Shearable {
         this.setSheared();
         int i = 1 + this.random.nextInt(3);
         for (int j = 0; j < i; ++j) {
-            ItemEntity itemEntity = this.spawnAtLocation(EstrogenItems.MOTH_FUZZ.get(), 1);
+            ItemEntity itemEntity = this.spawnAtLocation(EstrogenItems.MOTH_FUZZ.get().getDefaultInstance(), 0.5f);
             if (itemEntity == null) continue;
             itemEntity.setDeltaMovement(itemEntity.getDeltaMovement().add((this.random.nextFloat() - this.random.nextFloat()) * 0.1f, this.random.nextFloat() * 0.05f, (this.random.nextFloat() - this.random.nextFloat()) * 0.1f));
         }
