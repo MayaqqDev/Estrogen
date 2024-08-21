f87760edcf97f9873caa3a8d47c84accb8811348
MayaqqDev <i@mayaqq.dev>
2024-08-21 03:19:34 +0200

    fixed sound subtitles, added ambient moth sound and found a minecraft bug. Also new moth elytra texture

--- common/src/main/java/dev/mayaqq/estrogen/registry/EstrogenSounds.java
+++ common/src/main/java/dev/mayaqq/estrogen/registry/EstrogenSounds.java
@@ -31,12 +31,17 @@ public class EstrogenSounds {
     public static final RegistryEntry<SoundEvent> DREAM_BLOCK_LOOP = SOUNDS.register("dream_block_loop", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_loop")));
     public static final RegistryEntry<SoundEvent> DREAM_BLOCK_EXIT = SOUNDS.register("dream_block_exit", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_exit")));
     public static final RegistryEntry<SoundEvent> DREAM_BLOCK_PLACE = SOUNDS.register("dream_block_place", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_place")));
+    public static final RegistryEntry<SoundEvent> DREAM_BLOCK_BREAK = SOUNDS.register("dream_block_break", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_break")));
+    public static final RegistryEntry<SoundEvent> DREAM_BLOCK_STEP = SOUNDS.register("dream_block_step", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_step")));
+    public static final RegistryEntry<SoundEvent> DREAM_BLOCK_FALL = SOUNDS.register("dream_block_fall", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_fall")));
+    public static final RegistryEntry<SoundEvent> DREAM_BLOCK_HIT = SOUNDS.register("dream_block_hit", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_hit")));
 
     // Dormant Dream Block
     public static final RegistryEntry<SoundEvent> DREAM_BLOCK_DORMANT_BREAK = SOUNDS.register("dream_block_dormant_break", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_dormant_break")));
     public static final RegistryEntry<SoundEvent> DREAM_BLOCK_DORMANT_PLACE = SOUNDS.register("dream_block_dormant_place", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_dormant_place")));
     public static final RegistryEntry<SoundEvent> DREAM_BLOCK_DORMANT_STEP = SOUNDS.register("dream_block_dormant_step", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_dormant_step")));
     public static final RegistryEntry<SoundEvent> DREAM_BLOCK_DORMANT_FALL = SOUNDS.register("dream_block_dormant_fall", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_dormant_fall")));
+    public static final RegistryEntry<SoundEvent> DREAM_BLOCK_DORMANT_HIT = SOUNDS.register("dream_block_dormant_hit", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("dream_block_dormant_hit")));
 
     // Cookie Jar
     public static final RegistryEntry<SoundEvent> JAR_FULL = SOUNDS.register("jar_full", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("jar_full")));
@@ -44,9 +49,19 @@ public class EstrogenSounds {
     public static final RegistryEntry<SoundEvent> JAR_BREAK = SOUNDS.register("jar_break", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("jar_break")));
     public static final RegistryEntry<SoundEvent> JAR_STEP = SOUNDS.register("jar_step", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("jar_step")));
     public static final RegistryEntry<SoundEvent> JAR_INSERT = SOUNDS.register("jar_insert", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("jar_insert")));
+    public static final RegistryEntry<SoundEvent> JAR_HIT = SOUNDS.register("jar_hit", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("jar_hit")));
+    public static final RegistryEntry<SoundEvent> JAR_FALL = SOUNDS.register("jar_fall", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("jar_fall")));
 
     // Pill Box
     public static final RegistryEntry<SoundEvent> PILL_BOX_PLACE = SOUNDS.register("pill_box_place", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("pill_box_place")));
     public static final RegistryEntry<SoundEvent> PILL_BOX_BREAK = SOUNDS.register("pill_box_break", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("pill_box_break")));
     public static final RegistryEntry<SoundEvent> PILL_BOX_STEP = SOUNDS.register("pill_box_step", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("pill_box_step")));
+    public static final RegistryEntry<SoundEvent> PILL_BOX_HIT = SOUNDS.register("pill_box_hit", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("pill_box_hit")));
+    public static final RegistryEntry<SoundEvent> PILL_BOX_FALL = SOUNDS.register("pill_box_fall", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("pill_box_fall")));
+
+    // Moth
+    public static final RegistryEntry<SoundEvent> MOTH_DEATH = SOUNDS.register("moth_death", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("moth_death")));
+    public static final RegistryEntry<SoundEvent> MOTH_HURT = SOUNDS.register("moth_hurt", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("moth_hurt")));
+    public static final RegistryEntry<SoundEvent> MOTH_LOOP = SOUNDS.register("moth_loop", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("moth_loop")));
+    public static final RegistryEntry<SoundEvent> MOTH_FUZZ_UP = SOUNDS.register("moth_fuzz_up", () -> SoundEvent.createVariableRangeEvent(Estrogen.id("moth_fuzz_up")));
 }
