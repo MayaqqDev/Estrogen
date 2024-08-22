2f498ac93375e87b67fd209dadbf51d040006e8b
sery <sery@sappho.company>
2024-08-22 19:57:27 +0200

    should probably put executor there

--- fabric/src/main/java/dev/mayaqq/estrogen/fabric/client/EstrogenClientFabric.java
+++ fabric/src/main/java/dev/mayaqq/estrogen/fabric/client/EstrogenClientFabric.java
@@ -33,8 +33,8 @@ public class EstrogenClientFabric implements ClientModInitializer {
         // Pretty fabric way using model loading plugin api
         PreparableModelLoadingPlugin.register(
             (resourceManager, executor) -> CompletableFuture.supplyAsync(() ->
-                LocationResolver.load(resourceManager, "models/thigh_high_styles", "models", ".json")
-            ), (data, pluginContext) -> pluginContext.addModels(data.locations())
+                LocationResolver.load(resourceManager, "models/thigh_high_styles", "models", ".json"), executor),
+            (data, pluginContext) -> pluginContext.addModels(data.locations())
         );
     }
 
