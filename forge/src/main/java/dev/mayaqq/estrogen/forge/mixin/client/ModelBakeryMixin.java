437a026dfca63c79194cb92a6db76aec40904e43
sery <sery@sappho.company>
2024-08-24 18:51:07 +0200

    switch to WrapOperation from Redirect

--- forge/src/main/java/dev/mayaqq/estrogen/forge/mixin/client/ModelBakeryMixin.java
+++ forge/src/main/java/dev/mayaqq/estrogen/forge/mixin/client/ModelBakeryMixin.java
@@ -1,11 +1,12 @@
 package dev.mayaqq.estrogen.forge.mixin.client;
 
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import dev.mayaqq.estrogen.Estrogen;
 import dev.mayaqq.estrogen.forge.client.models.ForgeThighHighItemModel;
 import dev.mayaqq.estrogen.utils.client.EstrogenClientPaths;
 import net.minecraft.client.color.block.BlockColors;
 import net.minecraft.client.renderer.block.model.BlockModel;
-import net.minecraft.client.resources.TextureAtlasHolder;
 import net.minecraft.client.resources.model.ModelBakery;
 import net.minecraft.client.resources.model.UnbakedModel;
 import net.minecraft.resources.FileToIdConverter;
@@ -16,9 +17,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-
 import java.io.IOException;
 import java.util.Map;
 
@@ -56,18 +55,18 @@ public abstract class ModelBakeryMixin {
         });
     }
 
-    @Redirect(
+    @WrapOperation(
         method = "loadTopLevel",
         at = @At(
             value = "INVOKE",
             target = "Lnet/minecraft/client/resources/model/ModelBakery;getModel(Lnet/minecraft/resources/ResourceLocation;)Lnet/minecraft/client/resources/model/UnbakedModel;"
         )
     )
-    public UnbakedModel wrapModel(ModelBakery instance, ResourceLocation modelLocation) {
+    public UnbakedModel wrapModel(ModelBakery instance, ResourceLocation modelLocation, Operation<UnbakedModel> original) {
         if(modelLocation.equals(EstrogenClientPaths.THIGH_HIGH_ITEM_LOCATION)) {
-            return new ForgeThighHighItemModel(getModel(modelLocation));
+            return new ForgeThighHighItemModel(original.call(instance, modelLocation));
         }
-        return getModel(modelLocation);
+        return original.call(instance, modelLocation);
     }
 
 
