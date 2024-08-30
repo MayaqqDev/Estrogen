8202ee84684874a022731c48a605dd844d348b33
sery <sery@sappho.company>
2024-08-30 10:30:30 +0200

    little change :3

--- common/src/main/java/dev/mayaqq/estrogen/registry/tooltip/ThighHighsToolTipModifier.java
+++ common/src/main/java/dev/mayaqq/estrogen/registry/tooltip/ThighHighsToolTipModifier.java
@@ -5,18 +5,25 @@ import dev.mayaqq.estrogen.registry.EstrogenItems;
 import dev.mayaqq.estrogen.registry.items.ThighHighsItem;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 
 import java.util.List;
 
 public class ThighHighsToolTipModifier implements TooltipModifier {
+
+    private final ThighHighsItem item;
+
+    public ThighHighsToolTipModifier(Item item) {
+        this.item = (ThighHighsItem) item;
+    }
+
     @Override
     public void modify(ItemStack stack, Player player, TooltipFlag flags, List<Component> tooltip) {
-        ThighHighsItem thighHighs = EstrogenItems.THIGH_HIGHS.get();
-        if(!stack.is(thighHighs)) return;
+        if(!stack.is(item)) return;
 
-        thighHighs.getStyle(stack).ifPresent(style -> {
+        item.getStyle(stack).ifPresent(style -> {
             String translationKey = style.toLanguageKey("tooltip.thigh_highs");
             tooltip.add(1, Component.translatable(translationKey));
         });
