2e3f02c17c9d2e9452602ab776aa950bfd09fa2a
sery <sery@sappho.company>
2024-09-08 07:17:35 +0200

    frapi support

--- common/src/main/java/dev/mayaqq/estrogen/utils/client/ItemModelBufferer.java
+++ common/src/main/java/dev/mayaqq/estrogen/utils/client/ItemModelBufferer.java
@@ -4,15 +4,18 @@ import com.jozufozu.flywheel.core.model.BlockModel;
 import com.jozufozu.flywheel.core.model.ModelUtil;
 import com.jozufozu.flywheel.core.model.ShadeSeparatedBufferedData;
 import com.mojang.blaze3d.vertex.PoseStack;
-import dev.mayaqq.estrogen.platform.ClientPlatform;
+import com.mojang.blaze3d.vertex.VertexConsumer;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LightTexture;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.ItemRenderer;
 import net.minecraft.client.renderer.texture.OverlayTexture;
-import net.minecraft.client.resources.model.BakedModel;
 import net.minecraft.world.item.ItemDisplayContext;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+
+import java.util.List;
 
 public class ItemModelBufferer {
 
@@ -20,18 +23,81 @@ public class ItemModelBufferer {
         ItemRenderer itemRenderer = Minecraft.getInstance().getItemRenderer();
         ShadeSeparatedBufferedData data = ModelUtil.getBufferedData((consumer, renderer, random) -> {
             PoseStack poseStack = new PoseStack();
-            BakedModel model = itemRenderer.getModel(stack, level, null, random.nextInt());
+            GlintFilteringBufferSource bufferSource = new GlintFilteringBufferSource(consumer);
 
-            poseStack.pushPose();
-            ClientPlatform.applyItemTransforms(model, context, poseStack);
-            poseStack.translate(-0.5f, -0.5f, -0.5f);
-            itemRenderer.renderModelLists(model, stack, LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY, poseStack, consumer);
-            poseStack.popPose();
-
+            itemRenderer.renderStatic(stack, context,
+                LightTexture.FULL_BRIGHT, OverlayTexture.NO_OVERLAY,
+                poseStack, bufferSource, level, random.nextInt()
+            );
         });
 
         BlockModel model = new BlockModel(data, stack.getItem().toString());
         data.release();
         return model;
     }
+
+    private record GlintFilteringBufferSource(VertexConsumer wrapped) implements MultiBufferSource {
+
+            private static final VertexConsumer EMPTY = new VertexConsumer() {
+                @Override
+                public VertexConsumer vertex(double x, double y, double z) {
+                    return this;
+                }
+
+                @Override
+                public VertexConsumer color(int red, int green, int blue, int alpha) {
+                    return this;
+                }
+
+                @Override
+                public VertexConsumer uv(float u, float v) {
+                    return this;
+                }
+
+                @Override
+                public VertexConsumer overlayCoords(int u, int v) {
+                    return this;
+                }
+
+                @Override
+                public VertexConsumer uv2(int u, int v) {
+                    return this;
+                }
+
+                @Override
+                public VertexConsumer normal(float x, float y, float z) {
+                    return this;
+                }
+
+                @Override
+                public void endVertex() {
+
+                }
+
+                @Override
+                public void defaultColor(int defaultR, int defaultG, int defaultB, int defaultA) {
+
+                }
+
+                @Override
+                public void unsetDefaultColor() {
+
+                }
+            };
+
+        private static final List<RenderType> GLINT_TYPES = List.of(
+                RenderType.glint(),
+                RenderType.glintDirect(),
+                RenderType.glintTranslucent(),
+                RenderType.entityGlint(),
+                RenderType.entityGlintDirect(),
+                RenderType.armorGlint(),
+                RenderType.armorEntityGlint()
+            );
+
+        @Override
+            public VertexConsumer getBuffer(RenderType renderType) {
+                return (GLINT_TYPES.contains(renderType)) ? EMPTY : wrapped;
+            }
+        }
 }
