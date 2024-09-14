fb7c27ad3ad0b0d46e82aba25afecbb468c1ac4f
sery <sery@sappho.company>
2024-09-14 17:42:56 +0200

    add optifine check to shader helper

--- common/src/main/java/dev/mayaqq/estrogen/client/ShaderHelper.java
+++ common/src/main/java/dev/mayaqq/estrogen/client/ShaderHelper.java
@@ -1,10 +1,14 @@
 package dev.mayaqq.estrogen.client;
 
 import earth.terrarium.botarium.util.CommonHooks;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 import net.irisshaders.iris.api.v0.IrisApi;
 
+import java.lang.reflect.Field;
 import java.util.function.BooleanSupplier;
 
+@Environment(EnvType.CLIENT)
 public class ShaderHelper {
 
     private static BooleanSupplier shaderPackInUse;
@@ -15,11 +19,29 @@ public class ShaderHelper {
     }
 
     public static void init() {
-        if(CommonHooks.isModLoaded("iris") || CommonHooks.isModLoaded("oculus")) {
+        if (CommonHooks.isModLoaded("iris") || CommonHooks.isModLoaded("oculus")) {
             shaderPackInUse = () -> IrisApi.getInstance().isShaderPackInUse();
+        } else if (Package.getPackage("net.optifine") != null) {
+            shaderPackInUse = optifineShadersInUse();
         } else {
-            // I should probably account for canvas but it probably works there anyway so eh
             shaderPackInUse = () -> false;
+        }
+    }
+
+    private static BooleanSupplier optifineShadersInUse() {
+        try {
+            Class<?> ofShaders = Class.forName("net.optifine.shaders.Shaders");
+            Field field = ofShaders.getDeclaredField("shaderPackLoaded");
+            field.setAccessible(true);
+            return () -> {
+                try {
+                    return field.getBoolean(null);
+                } catch (IllegalAccessException ignored) {
+                    return false;
+                }
+            };
+        } catch (Exception ignored) {
+            return () -> false;
         }
     }
 
