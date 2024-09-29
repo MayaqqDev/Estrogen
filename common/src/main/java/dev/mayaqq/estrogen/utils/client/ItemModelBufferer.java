a171b1b12e086e68ea141c9bf77d637c6df3efc9
sery <sery@sappho.company>
2024-09-29 10:30:07 +0200

    move stuff around

--- common/src/main/java/dev/mayaqq/estrogen/utils/client/ItemModelBufferer.java
+++ common/src/main/java/dev/mayaqq/estrogen/utils/client/ItemModelBufferer.java
@@ -1,9 +1,7 @@
 package dev.mayaqq.estrogen.utils.client;
 
 import com.jozufozu.flywheel.core.model.BlockModel;
-import com.jozufozu.flywheel.core.model.ModelUtil;
 import com.mojang.blaze3d.vertex.*;
-import it.unimi.dsi.fastutil.objects.ObjectSet;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.client.renderer.MultiBufferSource;
@@ -17,6 +15,63 @@ import net.minecraft.world.level.Level;
 import java.util.Set;
 
 public class ItemModelBufferer {
+
+    private static final VertexConsumer EMPTY_CONSUMER = new VertexConsumer() {
+        @Override
+        public VertexConsumer vertex(double x, double y, double z) {
+            return this;
+        }
+
+        @Override
+        public VertexConsumer color(int red, int green, int blue, int alpha) {
+            return this;
+        }
+
+        @Override
+        public VertexConsumer uv(float u, float v) {
+            return this;
+        }
+
+        @Override
+        public VertexConsumer overlayCoords(int u, int v) {
+            return this;
+        }
+
+        @Override
+        public VertexConsumer uv2(int u, int v) {
+            return this;
+        }
+
+        @Override
+        public VertexConsumer normal(float x, float y, float z) {
+            return this;
+        }
+
+        @Override
+        public void endVertex() {}
+
+        @Override
+        public void defaultColor(int defaultR, int defaultG, int defaultB, int defaultA) {}
+
+        @Override
+        public void unsetDefaultColor() {}
+
+        @Override
+        public void putBulkData(PoseStack.Pose poseEntry, BakedQuad quad, float red, float green, float blue, int combinedLight, int combinedOverlay) {}
+
+        @Override
+        public void putBulkData(PoseStack.Pose poseEntry, BakedQuad quad, float[] colorMuls, float red, float green, float blue, int[] combinedLights, int combinedOverlay, boolean mulColor) {}
+    };
+
+    private static final Set<RenderType> GLINT_TYPES = Set.of(
+        RenderType.glint(),
+        RenderType.glintDirect(),
+        RenderType.glintTranslucent(),
+        RenderType.entityGlint(),
+        RenderType.entityGlintDirect(),
+        RenderType.armorGlint(),
+        RenderType.armorEntityGlint()
+    );
 
     private static final ThreadLocal<ThreadLocalObjects> LOCAL_OBJECTS = ThreadLocal.withInitial(ThreadLocalObjects::new);
 
@@ -64,66 +119,9 @@ public class ItemModelBufferer {
     // This fixes visual bugs as enchantment glint isn't currently supported by flywheel
     private record GlintFilteringBufferSource(VertexConsumer wrapped) implements MultiBufferSource {
 
-            private static final VertexConsumer EMPTY = new VertexConsumer() {
-                @Override
-                public VertexConsumer vertex(double x, double y, double z) {
-                    return this;
-                }
-
-                @Override
-                public VertexConsumer color(int red, int green, int blue, int alpha) {
-                    return this;
-                }
-
-                @Override
-                public VertexConsumer uv(float u, float v) {
-                    return this;
-                }
-
-                @Override
-                public VertexConsumer overlayCoords(int u, int v) {
-                    return this;
-                }
-
-                @Override
-                public VertexConsumer uv2(int u, int v) {
-                    return this;
-                }
-
-                @Override
-                public VertexConsumer normal(float x, float y, float z) {
-                    return this;
-                }
-
-                @Override
-                public void endVertex() {}
-
-                @Override
-                public void defaultColor(int defaultR, int defaultG, int defaultB, int defaultA) {}
-
-                @Override
-                public void unsetDefaultColor() {}
-
-                @Override
-                public void putBulkData(PoseStack.Pose poseEntry, BakedQuad quad, float red, float green, float blue, int combinedLight, int combinedOverlay) {}
-
-                @Override
-                public void putBulkData(PoseStack.Pose poseEntry, BakedQuad quad, float[] colorMuls, float red, float green, float blue, int[] combinedLights, int combinedOverlay, boolean mulColor) {}
-            };
-
-        private static final Set<RenderType> GLINT_TYPES = Set.of(
-                RenderType.glint(),
-                RenderType.glintDirect(),
-                RenderType.glintTranslucent(),
-                RenderType.entityGlint(),
-                RenderType.entityGlintDirect(),
-                RenderType.armorGlint(),
-                RenderType.armorEntityGlint()
-            );
-
         @Override
         public VertexConsumer getBuffer(RenderType renderType) {
-            return GLINT_TYPES.contains(renderType) ? EMPTY : wrapped;
+            return GLINT_TYPES.contains(renderType) ? EMPTY_CONSUMER : wrapped;
         }
     }
 }
