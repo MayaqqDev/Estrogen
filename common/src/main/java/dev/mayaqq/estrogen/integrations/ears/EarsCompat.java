b25b9cc04a4c4f41de97ba228243ddde8b392d76
ThatGravyBoat <thatgravyboat@gmail.com>
2024-10-01 15:31:59 -0230

    Gender Change Potion

--- common/src/main/java/dev/mayaqq/estrogen/integrations/ears/EarsCompat.java
+++ common/src/main/java/dev/mayaqq/estrogen/integrations/ears/EarsCompat.java
@@ -4,7 +4,7 @@ import com.unascribed.ears.api.EarsFeatureType;
 import com.unascribed.ears.api.registry.EarsInhibitorRegistry;
 import dev.mayaqq.estrogen.config.EstrogenConfig;
 import dev.mayaqq.estrogen.config.PlayerEntityExtension;
-import dev.mayaqq.estrogen.registry.EstrogenEffects;
+import dev.mayaqq.estrogen.utils.Boob;
 import net.minecraft.world.entity.player.Player;
 
 public class EarsCompat {
@@ -13,7 +13,7 @@ public class EarsCompat {
         EarsInhibitorRegistry.register("estrogen", (part, peer) ->
                 EstrogenConfig.client().ears.get() &&
                 part == EarsFeatureType.CHEST &&
-                ((Player) peer).hasEffect(EstrogenEffects.ESTROGEN_EFFECT.get()) &&
+                Boob.shouldShow((Player) peer) &&
                 EstrogenConfig.client().chestFeatureRendering.get() &&
                 ((PlayerEntityExtension) peer).estrogen$getChestConfig() != null &&
                 ((PlayerEntityExtension) peer).estrogen$getChestConfig().enabled()
