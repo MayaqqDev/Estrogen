3db2d529936a0c2713477cc4758e44aecab6323d
MayaqqDev <i@mayaqq.dev>
2024-10-11 18:49:42 +0200

    Made boobs a little bigger

--- common/src/main/java/dev/mayaqq/estrogen/utils/EstrogenMath.java
+++ common/src/main/java/dev/mayaqq/estrogen/utils/EstrogenMath.java
@@ -4,6 +4,6 @@ import net.minecraft.util.Mth;
 
 public class EstrogenMath {
     public static float boobFunc(float level) {
-        return 1.26F - Mth.invSqrt((level + 0.6F)*0.95F);
+        return 1.48F - Mth.invSqrt((level + 0.6F)*0.95F);
     }
 }
