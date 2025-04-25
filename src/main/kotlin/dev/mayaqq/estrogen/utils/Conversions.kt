9dd54fd255b7a9ea347281b6eb23d7743d181657
Mayaqq <i@mayaqq.dev>
2025-04-25 19:24:41 +0200

    Useless Conversions
    Signed-off-by: Mayaqq <i@mayaqq.dev>

--- src/main/kotlin/dev/mayaqq/estrogen/utils/Conversions.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/utils/Conversions.kt
@@ -1,6 +1,24 @@
 package dev.mayaqq.estrogen.utils
 
 import net.minecraft.core.BlockPos
+import net.minecraft.core.Vec3i
+import net.minecraft.world.phys.Vec3
+import org.joml.Vector3d
 import org.joml.Vector3f
 
+// TODO: Once bigger put this in cynosure
 fun Vector3f.blockPos() = BlockPos(this.x.toInt(), this.y.toInt(), this.z.toInt())
+fun Vector3d.blockPos() = BlockPos(this.x.toInt(), this.y.toInt(), this.z.toInt())
+fun Vec3.blockPos() = BlockPos(this.x.toInt(), this.y.toInt(), this.z.toInt())
+fun Vec3i.blockPos() = BlockPos(this.x, this.y, this.z)
+
+fun BlockPos.toVector3f() = Vector3f(this.x.toFloat(), this.y.toFloat(), this.z.toFloat())
+fun BlockPos.toVector3d() = Vector3d(this.x.toDouble(), this.y.toDouble(), this.z.toDouble())
+fun BlockPos.toVec3() = Vec3(this.x.toDouble(), this.y.toDouble(), this.z.toDouble())
+
+fun Vector3f.toVector3d() = Vector3d(this.x.toDouble(), this.y.toDouble(), this.z.toDouble())
+fun Vector3d.toVector3f() = Vector3f(this.x.toFloat(), this.y.toFloat(), this.z.toFloat())
+
+fun Vector3f.toVec3() = Vec3(this.x.toDouble(), this.y.toDouble(), this.z.toDouble())
+fun Vector3d.toVec3() = Vec3(this.x, this.y, this.z)
+
