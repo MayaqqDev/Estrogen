a4ff8b4aefafdfc937115ea83d9afd1be1355045
TheRobbie72 <franc.mer2022@gamil.com>
2025-05-01 23:08:06 +0800

    disable jumping while dashing

--- src/main/java/dev/mayaqq/estrogen/mixin/PlayerMixin.java
+++ src/main/java/dev/mayaqq/estrogen/mixin/PlayerMixin.java
@@ -1,18 +1,29 @@
 package dev.mayaqq.estrogen.mixin;
 
 import dev.mayaqq.estrogen.config.types.ChestConfig;
+import dev.mayaqq.estrogen.features.dash.CommonDash;
 import dev.mayaqq.estrogen.injection.IPlayer;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
-public class PlayerMixin implements IPlayer {
+public abstract class PlayerMixin extends LivingEntity implements IPlayer {
 
     @Unique
     @Nullable
     private ChestConfig estrogen$chestConfig;
+
+    protected PlayerMixin(EntityType<? extends LivingEntity> entityType, Level level) {
+        super(entityType, level);
+    }
 
     @Override
     public @Nullable ChestConfig estrogen$getChestConfig() {
@@ -22,5 +33,10 @@ public class PlayerMixin implements IPlayer {
     @Override
     public void estrogen$setChestConfig(@Nullable ChestConfig config) {
         estrogen$chestConfig = config;
+    }
+
+    @Inject(method = "jumpFromGround()V", at = @At("HEAD"), cancellable = true)
+    private void jumpFromGround(CallbackInfo callbackInfo) {
+        if (CommonDash.INSTANCE.isDashing(getUUID())) callbackInfo.cancel();
     }
 }
