b7b000515d081011f6a22e5557977133f73d519e
Serenity <166204436+serenyadev@users.noreply.github.com>
2025-05-05 20:59:36 +0200

    delete old common

--- common/src/main/java/dev/mayaqq/estrogen/client/entity/player/features/boobs/TextureData.java
+++
@@ -1,5 +0,0 @@
-package dev.mayaqq.estrogen.client.entity.player.features.boobs;
-
-import net.minecraft.resources.ResourceLocation;
-
-public record TextureData(ResourceLocation location, Float u, Float v, Float leftU, Float leftV, Float rightU, Float rightV, Float textureWidth, Float textureHeight) {}
