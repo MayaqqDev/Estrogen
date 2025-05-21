dd8c415171afe4ec646d4453ba93208b2d57ac51
Mayaqq <i@mayaqq.dev>
2025-05-21 14:32:04 +0200

    compacted bc its like useless lol

--- src/forge/kotlin/dev/mayaqq/estrogen/forge/compat/ReiForgeCompat.kt
+++ src/forge/kotlin/dev/mayaqq/estrogen/forge/compat/ReiForgeCompat.kt
@@ -1,31 +1,19 @@
 package dev.mayaqq.estrogen.forge.compat
 
 import dev.mayaqq.estrogen.compat.rei.ReiEstrogenPlugin
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin
 import me.shedaniel.rei.api.common.plugins.PluginView
 import me.shedaniel.rei.api.common.plugins.REIPlugin
 import me.shedaniel.rei.api.common.plugins.REIPluginProvider
-import java.lang.String.join
 import java.util.Collections
 
+/*
+ This is a fucked up way to go around REI only having the annotation on the forge side... (Fuck you REI)
+ */
 fun registerPlugin() {
-    PluginView.getClientInstance().registerPlugin(wrapPlugin(Collections.singletonList("estrogen"), ReiEstrogenPlugin))
-}
-
-private fun <P : REIPlugin<*>?> wrapPlugin(modId: List<String>, plugin: REIPluginProvider<P>): REIPluginProvider<P> {
-    return object : REIPluginProvider<P> {
-        val nameSuffix: String = " [" + join(", ", modId) + "]"
-
-        override fun provide(): Collection<P> {
-            return plugin.provide()
-        }
-
-        override fun getPluginProviderClass(): Class<P> {
-            return plugin.pluginProviderClass
-        }
-
-        override fun getPluginProviderName(): String {
-            val var10000 = plugin.pluginProviderName
-            return var10000 + this.nameSuffix
-        }
-    }
+    PluginView.getClientInstance().registerPlugin(object : REIPluginProvider<REIClientPlugin> {
+        override fun provide(): Collection<REIClientPlugin> = ReiEstrogenPlugin.provide()
+        override fun getPluginProviderClass(): Class<REIClientPlugin> = ReiEstrogenPlugin.pluginProviderClass
+        override fun getPluginProviderName(): String = ReiEstrogenPlugin.pluginProviderName + " [estrogen]"
+    })
 }
