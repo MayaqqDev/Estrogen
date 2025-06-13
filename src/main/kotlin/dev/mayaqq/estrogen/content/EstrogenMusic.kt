99f709a082ac26347968c6051883ae199ce27def
Mayaqq <i@mayaqq.dev>
2025-06-13 11:29:46 +0200

    Musiced

--- src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenMusic.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenMusic.kt
@@ -1,10 +1,19 @@
 package dev.mayaqq.estrogen.content
 
+import dev.mayaqq.cynosure.music.register
+import dev.mayaqq.estrogen.config.EstrogenClientConfig
+import dev.mayaqq.estrogen.content.EstrogenEffects.Estrogen
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.sounds.Music
 import net.minecraft.sounds.Musics
 
 object EstrogenMusic : Musics() {
     @JvmField
-    val EstrogenAmbient: Music = Music(BuiltInRegistries.SOUND_EVENT.wrapAsHolder(EstrogenSounds.ESTROGEN_AMBIENT), 0, 0, true)
+    val EstrogenAmbient: Music = Music(
+        BuiltInRegistries.SOUND_EVENT.wrapAsHolder(EstrogenSounds.ESTROGEN_AMBIENT),
+        0,
+        0,
+        true).register { player, manager, biome ->
+            EstrogenClientConfig.ambientMusic && player.hasEffect(Estrogen)
+        }
 }
