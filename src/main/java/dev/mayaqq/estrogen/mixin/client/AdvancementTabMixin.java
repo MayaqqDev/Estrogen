0319202dff26326dc3d4e27c22fe2d653bf085f8
TheRobbie72 <franc.mer2022@gamil.com>
2025-07-22 00:06:57 +0800

    use shader only when display background == "estrogen:textures/block/dream_block/particle.png"

--- src/main/java/dev/mayaqq/estrogen/mixin/client/AdvancementTabMixin.java
+++ src/main/java/dev/mayaqq/estrogen/mixin/client/AdvancementTabMixin.java
@@ -6,25 +6,35 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.*;
 import dev.mayaqq.estrogen.client.content.EstrogenRenderer;
 import dev.mayaqq.estrogen.client.content.blockRenderers.dreamBlock.texture.DynamicDreamTexture;
+import net.minecraft.advancements.DisplayInfo;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.advancements.AdvancementTab;
 import net.minecraft.client.renderer.LightTexture;
 import net.minecraft.resources.ResourceLocation;
 import org.joml.Matrix4f;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.Objects;
 
 
 @Mixin(AdvancementTab.class)
 public class AdvancementTabMixin {
+    @Shadow @Final private DisplayInfo display;
+
     @WrapOperation(
             method = "drawContents",
             at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;blit(Lnet/minecraft/resources/ResourceLocation;IIFFIIII)V")
     )
     private void shaderBG(GuiGraphics instance, ResourceLocation atlasLocation, int x, int y, float uOffset, float vOffset, int width, int height, int textureWidth, int textureHeight, Operation<Void> original) {
-        // original.call(instance, atlasLocation, x, y, uOffset, vOffset, width, height, textureWidth, textureHeight);
-        estrogen$renderDream(instance, x, x + width, y, y + height);
+        if (this.display.getBackground() != null && this.display.getBackground().toString().equals("estrogen:textures/block/dream_block/particle.png")) {
+            estrogen$renderDream(instance, x, x + width, y, y + height);
+        } else {
+            original.call(instance, atlasLocation, x, y, uOffset, vOffset, width, height, textureWidth, textureHeight);
+        }
     }
 
     @Unique
