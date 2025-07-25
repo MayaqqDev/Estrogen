9f0c16f9c3d1072674d25644a03e41cc05373d22
Mayaqq <i@mayaqq.dev>
2025-07-25 21:27:20 +0200

    Sponging my horse urine rn so hard

---
+++ src/main/java/dev/mayaqq/estrogen/mixin/SpongeBlockMixin.java
@@ -0,0 +1,41 @@
+package dev.mayaqq.estrogen.mixin;
+
+import dev.mayaqq.estrogen.content.EstrogenFluids;
+import earth.terrarium.botarium.common.fluid.base.BotariumFluidBlock;
+import earth.terrarium.botarium.common.registry.fluid.BotariumLiquidBlock;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.SpongeBlock;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(SpongeBlock.class)
+public class SpongeBlockMixin {
+    @Inject(
+            method = "lambda$removeWaterBreadthFirstSearch$1",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/world/level/Level;getFluidState(Lnet/minecraft/core/BlockPos;)Lnet/minecraft/world/level/material/FluidState;",
+                    shift = At.Shift.AFTER
+            ),
+            cancellable = true
+    )
+    private static void inject(BlockPos center, Level level, BlockPos pos, CallbackInfoReturnable<Boolean> cir) {
+        BlockState blockState = level.getBlockState(pos);
+        FluidState fluidState = level.getFluidState(pos);
+        if (
+            (fluidState.is(EstrogenFluids.INSTANCE.getHorseUrine().getSource()) || fluidState.is(EstrogenFluids.INSTANCE.getHorseUrine().getFlowing()))
+            && blockState.hasProperty(BotariumLiquidBlock.LEVEL)) {
+            level.setBlockAndUpdate(
+                    pos,
+                    EstrogenFluids.INSTANCE.getFiltratedHorseUrine().getBlock().withPropertiesOf(blockState)
+            );
+            cir.setReturnValue(true);
+        }
+    }
+}
