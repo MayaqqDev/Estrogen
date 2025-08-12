84d18f784744656b635037e0fc22e6caa409005d
Mayaqq <i@mayaqq.dev>
2025-08-12 13:08:28 +0900

    Components are hard lol, also deleted unused Centrifuge stuff

--- src/main/kotlin/dev/mayaqq/estrogen/utils/transfer/TransferHelper.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/utils/transfer/TransferHelper.kt
@@ -1,22 +1,38 @@
 package dev.mayaqq.estrogen.utils.transfer
 
+import net.minecraft.ChatFormatting
 import net.minecraft.network.chat.ClickEvent
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.OutgoingChatMessage
 import net.minecraft.world.level.Level
 
 object TransferHelper {
     fun message(level: Level) {
-        val message = Component.literal("The Estrogen mod has updated and has been split into \n")
+        val message = Component.literal("The Estrogen mod has updated and has been split into ")
+            .withStyle(ChatFormatting.RED)
             .append(
                 Component.literal("[Estrogen]").withStyle { it.withClickEvent(
-                    ClickEvent(ClickEvent.Action.OPEN_URL, "https://modrinth.com/mod/estrogen"))}
+                    ClickEvent(ClickEvent.Action.OPEN_URL, "https://modrinth.com/mod/estrogen"))
+                    .withColor(ChatFormatting.BLUE).withUnderlined(true)
+                }
             )
-            .append(" and ")
+            .append(Component.literal(" and ").withStyle(ChatFormatting.RED))
             .append(
                 Component.literal("[Create: Estrogen]").withStyle { it.withClickEvent(
-                    ClickEvent(ClickEvent.Action.OPEN_URL, "https://modrinth.com/mod/create-estrogen"))}
+                    ClickEvent(ClickEvent.Action.OPEN_URL, "https://modrinth.com/mod/create-estrogen"))
+                    .withColor(ChatFormatting.BLUE).withUnderlined(true)
+                }
             )
-            .append(", please install Create: Estrogen\nto fix this message and your world.")
-        level.server?.sendSystemMessage(message)
+            .append(Component.literal(", please install ").withStyle(ChatFormatting.RED))
+            .append(
+                Component.literal("Create: Estrogen").withStyle { it.withClickEvent(
+                    ClickEvent(ClickEvent.Action.OPEN_URL, "https://modrinth.com/mod/create-estrogen"))
+                    .withColor(ChatFormatting.BLUE).withUnderlined(true)
+                }
+            )
+            .append(Component.literal(" to fix this message and your world.").withStyle(ChatFormatting.RED))
+        level.server?.playerList?.players?.forEach { player ->
+            player.displayClientMessage(message, false)
+        }
     }
 }
