0bc1ed50dbe149fdd274ab4f8a6987804a5771eb
Mayaqq <i@mayaqq.dev>
2025-08-19 13:33:57 +0900

    block yay!!!

--- src/main/kotlin/dev/mayaqq/estrogen/compat/recipeviewers/recipes/SpongingRvRecipe.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/compat/recipeviewers/recipes/SpongingRvRecipe.kt
@@ -5,6 +5,7 @@ import dev.mayaqq.estrogen.client.content.textures.RecipeTextures
 import dev.mayaqq.estrogen.compat.recipeviewers.base.RVRecipe
 import dev.mayaqq.estrogen.compat.recipeviewers.base.Role
 import dev.mayaqq.estrogen.compat.recipeviewers.base.RvRecipeData
+import dev.mayaqq.estrogen.compat.recipeviewers.base.elements.GuiBlockRenderer
 import dev.mayaqq.estrogen.compat.recipeviewers.base.ingredient.RvIngredient
 import dev.mayaqq.estrogen.content.EstrogenRecipes
 import dev.mayaqq.estrogen.content.recipes.SpongingRecipe
@@ -12,13 +13,23 @@ import dev.mayaqq.estrogen.content.recipes.inventory.FluidData
 import dev.mayaqq.estrogen.mixin.client.accessor.LiquidBlockAccessor
 import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.world.item.Items
+import net.minecraft.world.level.block.Blocks
+import net.minecraft.world.phys.Vec3
 
 class SpongingRvRecipe(recipe: SpongingRecipe) : RVRecipe<SpongingRecipe>(recipe) {
     override fun init() {
-        addTexture(RecipeTextures.JEI_SHADOW, 62, 47)
-        addTexture(RecipeTextures.JEI_DOWN_ARROW, 74, 10)
-        addSlot(inputs()[0], 51, 5, Role.INPUT)
-        addSlot(outputs()[0], 132, 38, Role.OUTPUT)
+        addTexture(RecipeTextures.JEI_SHADOW, 62, 37)
+        addTexture(RecipeTextures.JEI_ARROW, 7 + 18 + 4, 32)
+        addTexture(RecipeTextures.JEI_ARROW, 152 - 42 - 4, 32)
+        addSlot(inputs()[0], 7, 28, Role.INPUT)
+        addSlot(outputs()[0], 152, 28, Role.OUTPUT)
+        addDrawable(78, 42, GuiBlockRenderer(
+            Blocks.SPONGE.defaultBlockState(),
+            null,
+            0, 0, 0,
+            Vec3(22.5, 45.0, 0.0),
+            20.0
+        ))
     }
 
     override fun inputs(): List<RvIngredient> = buildList {
