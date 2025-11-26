6db67c647c7f3dbdaa093606aa72a168727077f1
Mayaqq <i@mayaqq.dev>
2025-11-26 12:44:10 +0100

    Disabled some movement checks when having the Estrogen Effect

---
+++ src/main/java/dev/mayaqq/estrogen/mixin/ServerGamePacketListenerImplMixin.java
@@ -0,0 +1,28 @@
+package dev.mayaqq.estrogen.mixin;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import dev.mayaqq.estrogen.content.EstrogenEffects;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public class ServerGamePacketListenerImplMixin {
+    @Shadow
+    public ServerPlayer player;
+
+    @ModifyExpressionValue(
+            method = "handleMovePlayer",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/server/level/ServerPlayer;isChangingDimension()Z"
+            )
+    )
+    private boolean disableWhenOnEstrogen(boolean original) {
+        if (player.hasEffect(EstrogenEffects.getEstrogen())) return true;
+        return original;
+    }
+}
+
