7dc6e60db115b2c4a2c12ff99adf831947b123b0
Mayaqq <i@mayaqq.dev>
2025-11-28 18:52:58 +0100

    Fixed Entity interactions with deployers

--- src/main/kotlin/dev/mayaqq/estrogen/content/recipes/EntityInteractionRecipe.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/content/recipes/EntityInteractionRecipe.kt
@@ -111,7 +111,7 @@ private fun entityToEgg(entity: EntityType<*>): ItemStack? = SpawnEggItem.byId(e
 @Subscription
 fun onEntityInteraction(event: InteractionEvent.UseEntity) {
     if (event.result == null) event.result = InteractionResult.PASS
-    if (event.level is ServerLevel && event.phase == InteractionEvent.UseEntity.Phase.GENERAL) {
+    if (event.level is ServerLevel) {
         event.level.recipeManager.getAllRecipesFor(EstrogenRecipes.ENTITY_INTERACTION).forEach { recipe ->
             val data = InteractionData(event.getUsedStack(),  event.entity, event.player as ServerPlayer)
             if (recipe.matches(data, event.level)) {
