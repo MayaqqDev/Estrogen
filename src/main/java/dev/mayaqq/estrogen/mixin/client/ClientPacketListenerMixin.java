8d7af88da0913b427a4e25390bd7188925f2b0d0
Mayaqq <i@mayaqq.dev>
2025-12-09 12:59:20 +0100

    UwUfy curse moved to server event, fixing #330

--- src/main/java/dev/mayaqq/estrogen/mixin/client/ClientPacketListenerMixin.java
+++ src/main/java/dev/mayaqq/estrogen/mixin/client/ClientPacketListenerMixin.java
@@ -1,44 +1,20 @@
 package dev.mayaqq.estrogen.mixin.client;
 
-import dev.mayaqq.cynosure.utils.fun.UwUfy;
 import dev.mayaqq.estrogen.client.content.sounds.MothFlyingSoundInstance;
-import dev.mayaqq.estrogen.content.EstrogenEnchantments;
 import dev.mayaqq.estrogen.content.entities.MothEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.multiplayer.ClientPacketListener;
-import net.minecraft.client.player.LocalPlayer;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(ClientPacketListener.class)
 public class ClientPacketListenerMixin {
     @Shadow @Final private Minecraft minecraft;
-
-    /**
-     * This mixin uwufies chat messages sent by the player if the player has a helmet with the Uwufying Curse enchantment.
-     * Because we modify the String by itself, and do not replace any method calls, it should be compatible with most if not all mods.
-     */
-    @ModifyVariable(
-            method = "sendChat(Ljava/lang/String;)V",
-            at = @At(value = "HEAD"),
-            ordinal = 0,
-            argsOnly = true
-    )
-    private String modifyMessage(String message) {
-        LocalPlayer player = Minecraft.getInstance().player;
-        if (player != null && EnchantmentHelper.getEnchantments(player.getInventory().getArmor(3)).containsKey(EstrogenEnchantments.INSTANCE.getUwUfyingCurse())) {
-            message = UwUfy.uwufy(message);
-
-        }
-        return message;
-    }
 
     /**
      * This mixin adds a MothFlyingSoundInstance to the sound manager when a moth entity is added.
--- src/main/kotlin/dev/mayaqq/estrogen/content/enchantments/UwuCurseEnchantment.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/content/enchantments/UwuCurseEnchantment.kt
@@ -1,12 +1,29 @@
+@file:EventSubscriber
 package dev.mayaqq.estrogen.content.enchantments
 
+import dev.mayaqq.cynosure.events.api.EventSubscriber
+import dev.mayaqq.cynosure.events.api.Subscription
+import dev.mayaqq.cynosure.events.server.ServerChatEvent
+import dev.mayaqq.cynosure.text.Text.asComponent
+import dev.mayaqq.cynosure.utils.`fun`.uwufy
+import dev.mayaqq.estrogen.Estrogen
+import dev.mayaqq.estrogen.content.EstrogenEnchantments.UwUfyingCurse
 import net.minecraft.world.entity.EquipmentSlot
 import net.minecraft.world.item.enchantment.Enchantment
 import net.minecraft.world.item.enchantment.EnchantmentCategory
+import net.minecraft.world.item.enchantment.EnchantmentHelper
 
 class UwuCurseEnchantment(rarity: Rarity, category: EnchantmentCategory, vararg slots: EquipmentSlot) : Enchantment(rarity, category, slots) {
     override fun getMinCost(level: Int): Int = 25
     override fun getMaxCost(level: Int): Int = 50
     override fun isTreasureOnly(): Boolean = true
     override fun isCurse(): Boolean = true
+}
+
+@Subscription
+fun onChatMessage(event: ServerChatEvent) {
+    Estrogen.info("On Message")
+    if (EnchantmentHelper.getEnchantments(event.player.getItemBySlot(EquipmentSlot.HEAD)).containsKey(UwUfyingCurse)) {
+        event.message = event.rawText.uwufy().asComponent()
+    }
 }
