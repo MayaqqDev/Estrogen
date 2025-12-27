18552c0ee2cf744b5d38798b85364249cf1e29ca
Mayaqq <i@mayaqq.dev>
2025-12-27 23:11:25 +0100

    small modification ig

--- src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenPoiTypes.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenPoiTypes.kt
@@ -11,7 +11,7 @@ import uwu.serenity.kritter.api.Registrar
 import uwu.serenity.kritter.api.entry
 
 object EstrogenPoiTypes : Registrar<PoiType> by Estrogen..Registries.POINT_OF_INTEREST_TYPE {
-    val DreamCatcher: PoiType by entry("dreamcatcher", {PoiHelpers.poi(EstrogenBlocks.DreamCatcher)}) {
+    val DreamCatcher: PoiType by entry("dreamcatcher", PoiHelpers.poiFactory(EstrogenBlocks.DreamCatcher)) {
         onRegister { registerState(it) }
     }
 }
