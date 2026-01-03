8bd26ba67174fa83a08dccdec4315acf6f5613be
Mayaqq <i@mayaqq.dev>
2026-01-03 21:35:27 +0100

    I am stupid.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,12 @@
 # Estrogen `@VERSION@` `1.20.1` `Fabric & Forge`
 
 ### Changelog:
+- 5.0.3 never existed
 - Fixed Fabric Accesswidener typo
+- Fixed Moth Spawning
+- Fixed Clipping into Dreamblocks with camera
+- Fixed Minigame setting
+- Compatibility with some Advancement Screen mods
+- Fixed Issues with Connector Extras (RAHH)
+- POI modification now works on forge as well (you can now put villagers in moth beds)
+- Updated Cynosure to 0.1.13
--- src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenPoiTypes.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenPoiTypes.kt
@@ -2,8 +2,6 @@ package dev.mayaqq.estrogen.content
 
 import dev.mayaqq.cynosure.blocks.poi.PoiHelpers
 import dev.mayaqq.cynosure.blocks.poi.PoiHelpers.registerState
-import dev.mayaqq.cynosure.core.Loader
-import dev.mayaqq.cynosure.core.currentLoader
 import dev.mayaqq.estrogen.Estrogen
 import net.minecraft.core.registries.Registries
 import net.minecraft.world.entity.ai.village.poi.PoiType
@@ -11,7 +9,7 @@ import uwu.serenity.kritter.api.Registrar
 import uwu.serenity.kritter.api.entry
 
 object EstrogenPoiTypes : Registrar<PoiType> by Estrogen..Registries.POINT_OF_INTEREST_TYPE {
-    val DreamCatcher: PoiType by entry("dreamcatcher", PoiHelpers.poiFactory(EstrogenBlocks.DreamCatcher)) {
+    val DreamCatcher: PoiType by entry("dreamcatcher", {PoiHelpers.poi(EstrogenBlocks.DreamCatcher)}) {
         onRegister { registerState(it) }
     }
 }
