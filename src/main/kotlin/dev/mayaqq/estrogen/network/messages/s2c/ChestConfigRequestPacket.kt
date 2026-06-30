aaf6431919786c53ff676c9ff4bbad2c0427cf69
Mayaqq <i@mayaqq.dev>
2026-06-30 19:23:18 +0100

    feat: Forgot serializable 2

--- src/main/kotlin/dev/mayaqq/estrogen/network/messages/s2c/ChestConfigRequestPacket.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/network/messages/s2c/ChestConfigRequestPacket.kt
@@ -1,7 +1,9 @@
 package dev.mayaqq.estrogen.network.messages.s2c
 
 import dev.mayaqq.estrogen.config.types.ChestConfig
+import kotlinx.serialization.Serializable
 
+@Serializable
 class ChestConfigRequestPacket {
     fun handle() {
         ChestConfig.sync()
