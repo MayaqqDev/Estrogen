96918c43e4f1be5ee4c174c56055b26f6ea2f51f
Mayaqq <i@mayaqq.dev>
2026-06-30 19:22:22 +0100

    feat: Forgot serializable

--- src/main/kotlin/dev/mayaqq/estrogen/network/messages/s2c/FlapSyncPacket.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/network/messages/s2c/FlapSyncPacket.kt
@@ -1,12 +1,14 @@
 package dev.mayaqq.estrogen.network.messages.s2c
 
 import dev.mayaqq.estrogen.injection.flap
+import kotlinx.serialization.Serializable
 import net.minecraft.client.Minecraft
 import kotlin.uuid.ExperimentalUuidApi
 import kotlin.uuid.Uuid
 import kotlin.uuid.toJavaUuid
 
 @OptIn(ExperimentalUuidApi::class)
+@Serializable
 data class FlapSyncPacket(val flaps: Int, val player: Uuid) {
     fun handle() {
         val level = Minecraft.getInstance().level
