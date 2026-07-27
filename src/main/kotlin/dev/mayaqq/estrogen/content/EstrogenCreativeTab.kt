33bf63322b14d8700a57496bcc128d300631269a
Mayaqq <i@mayaqq.dev>
2026-07-27 21:37:59 +0200

    fix: Creative mode tab error

--- src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenCreativeTab.kt
+++ src/main/kotlin/dev/mayaqq/estrogen/content/EstrogenCreativeTab.kt
@@ -1,5 +1,7 @@
 package dev.mayaqq.estrogen.content
 
+import dev.mayaqq.cynosure.text.TextProperties.stripped
+import dev.mayaqq.estrogen.Estrogen
 import dev.mayaqq.estrogen.MOD_ID
 import dev.mayaqq.estrogen.content.EstrogenBlocks.CookieJar
 import dev.mayaqq.estrogen.content.EstrogenBlocks.DreamBlock
@@ -37,6 +39,7 @@ import dev.mayaqq.estrogen.utils.defaultInstance
 import invoke.kitty.kritter.registry.api.Registrar
 import invoke.kitty.kritter.registry.api.entry.RegistryEntry
 import invoke.kitty.kritter.registry.api.entry.holder
+import invoke.kitty.kritter.registry.block.item
 import invoke.kitty.kritter.registry.creativeTab.creativeTab
 import invoke.kitty.kritter.utils.extensions.asStack
 import net.minecraft.core.Holder
@@ -44,10 +47,12 @@ import net.minecraft.core.registries.BuiltInRegistries
 import net.minecraft.core.registries.Registries
 import net.minecraft.network.chat.Component
 import net.minecraft.world.item.CreativeModeTab
+import net.minecraft.world.item.Item
 import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
 import net.minecraft.world.item.alchemy.Potion
 import net.minecraft.world.item.alchemy.PotionContents
+import net.minecraft.world.level.ItemLike
 import net.minecraft.world.level.block.Block
 
 
@@ -57,48 +62,49 @@ object EstrogenCreativeTab : Registrar<CreativeModeTab> by Registrar(MOD_ID, Reg
     val Estrogen = creativeTab("estrogen") {
         title = Component.translatable("itemGroup.estrogen.estrogen")
         icon(EstrogenPill::asStack)
-        displayItems { 
-            accept(EstrogenPill)
-            accept(CrystalEstrogenPill)
-            accept(GenderChangePotion)
-            accept(Balls)
-            accept(MothFuzz)
-            accept(TestosteroneChunk)
-            accept(TestosteronePowder)
-            accept(EstrogenChipCookie)
-            accept(HorseUrineBottle)
-            acceptWithCount(EstrogenPatches.getOrThrow().getFullStack())
-            accept(EstrogenPatches)
-            accept(ThighHighs)
-            accept(MothElytra)
-            accept(ColonThree)
-            accept(CookieJar)
-            accept(DreamCatcher)
-            accept(DreamBlock.asStack())
-            accept(DreamBottle)
-            accept(EstrogenPillBlock)
-            accept(MothWool)
-            accept(QuiltedMothWool)
-            accept(MothCarpet)
-            accept(QuiltedMothCarpet)
-            accept(MothBed)
-            accept(QuiltedMothBed)
+        displayItems {
+            acceptWithCount(EstrogenPill)
+            acceptWithCount(CrystalEstrogenPill)
+            acceptWithCount(GenderChangePotion)
+            acceptWithCount(Balls)
+            acceptWithCount(MothFuzz)
+            acceptWithCount(TestosteroneChunk)
+            acceptWithCount(TestosteronePowder)
+            acceptWithCount(EstrogenChipCookie)
+            acceptWithCount(HorseUrineBottle)
+            acceptWithCount(EstrogenPatches.get().getFullStack())
+            acceptWithCount(EstrogenPatches)
+            acceptWithCount(ThighHighs)
+            acceptWithCount(MothElytra)
+            acceptWithCount(ColonThree)
+            acceptWithCount(CookieJar)
+            acceptWithCount(DreamCatcher)
+            acceptWithCount(DreamBlock.asStack())
+            acceptWithCount(DreamBottle)
+            acceptWithCount(EstrogenPillBlock)
+            acceptWithCount(MothWool)
+            acceptWithCount(QuiltedMothWool)
+            acceptWithCount(MothCarpet)
+            acceptWithCount(QuiltedMothCarpet)
+            acceptWithCount(MothBed)
+            acceptWithCount(QuiltedMothBed)
             acceptWithCount(tippedArrow(EstrogenPotions.EstrogenPotion))
-            accept(MoltenSlime.bucket)
-            accept(TestosteroneMixture.bucket)
-            accept(LiquidEstrogen.bucket)
-            accept(FiltratedHorseUrine.bucket)
-            accept(HorseUrine.bucket)
-            accept(MoltenAmethyst.bucket)
-            accept(GenderFluid.bucket)
-            //accept()
+            acceptWithCount(MoltenSlime.bucket)
+            acceptWithCount(TestosteroneMixture.bucket)
+            acceptWithCount(LiquidEstrogen.bucket)
+            acceptWithCount(FiltratedHorseUrine.bucket)
+            acceptWithCount(HorseUrine.bucket)
+            acceptWithCount(MoltenAmethyst.bucket)
+            acceptWithCount(GenderFluid.bucket)
             ThighHighs.get().styleItems.forEach(::acceptWithCount)
         }
     }
 }
 
-private fun <T : Block> CreativeModeTab.Output.accept(holder: RegistryEntry<T>) = this.accept(holder.getOrThrow())
+private fun <T : ItemLike> CreativeModeTab.Output.acceptWithCount(holder: RegistryEntry<T>) = acceptWithCount(holder.get().asStack())
 
 private fun CreativeModeTab.Output.acceptWithCount(stack: ItemStack) = accept(stack.apply { count = 1 })
+
+private fun CreativeModeTab.Output.acceptWithCount(item: Item) = acceptWithCount(item.defaultInstance)
 
 private fun tippedArrow(potion: RegistryEntry<Potion>): ItemStack = PotionContents.createItemStack(Items.TIPPED_ARROW, potion.holder).apply { count = 1 }
