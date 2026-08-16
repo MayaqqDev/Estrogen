3451d59e48bf88456ce362822da4c67ede0a88f0
Mayaqq <i@mayaqq.dev>
2026-08-16 13:47:32 -0400

    chore: Bucket capabilities

--- src/neoforge/kotlin/dev/mayaqq/estrogen/neoforge/EstrogenNeoForge.kt
+++ src/neoforge/kotlin/dev/mayaqq/estrogen/neoforge/EstrogenNeoForge.kt
@@ -1,10 +1,14 @@
 package dev.mayaqq.estrogen.neoforge
 
 import dev.mayaqq.estrogen.content.EstrogenFluids
+import dev.mayaqq.estrogen.content.fluids.registry.EstrogenFluidEntry
 import invoke.kitty.kritter.platform.Mod
 import invoke.kitty.kritter.platform.forge.EntrypointHandler
 import invoke.kitty.kritter.platform.forge.eventBus
+import net.neoforged.neoforge.capabilities.Capabilities
+import net.neoforged.neoforge.capabilities.RegisterCapabilitiesEvent
 import net.neoforged.neoforge.fluids.FluidType
+import net.neoforged.neoforge.fluids.capability.wrappers.FluidBucketWrapper
 import net.neoforged.neoforge.registries.DeferredRegister
 
 object EstrogenNeoForge {
@@ -16,5 +20,14 @@ object EstrogenNeoForge {
             .let { field ->
                 field.get(EstrogenFluids.fluidRegistry) as DeferredRegister<FluidType>
             }.register(mod.eventBus)
+
+        mod.eventBus.addListener(::registerCapabilities)
     }
+
+    fun registerCapabilities(event: RegisterCapabilitiesEvent) {
+        event.registerItem(Capabilities.FluidHandler.ITEM, { stack, context ->
+            FluidBucketWrapper(stack)
+        }, *EstrogenFluids.map { (it as EstrogenFluidEntry<*,*>).bucketEntry.get() }.toTypedArray())
+    }
+
 }
