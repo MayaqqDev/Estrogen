da9975b727f81f5ca98e751fe83301a183c16846
Mayaqq <i@mayaqq.dev>
2026-08-24 20:20:17 +0200

    fix: Upsie

--- build.gradle.kts
+++ build.gradle.kts
@@ -333,7 +333,7 @@ cloche {
             modApi(libs.forge.rlib)
             modApi(libs.forge.csr)
             modCompileOnlyApi(libs.forge.curios.api())
-            modRuntimeOnly("org.sinytra.forgified-fabric-api:forgified-fabric-api:0.116.7+2.2.4+1.21.1")
+            //modRuntimeOnly("org.sinytra.forgified-fabric-api:forgified-fabric-api:0.116.7+2.2.4+1.21.1")
 
             include(libs.forge.rlib) { exclude(group = "com.teamresourceful", module = "bytecodecs") }
             include(libs.forge.mixinExtras) { isTransitive = false }
